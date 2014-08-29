010f401da817454f8d382ff6ebc3d7ebae59c622
Sai Gudigundla <saip106@msn.com>
2014-08-28 21:43:47 -0400

    generated angular app using yeoman generator

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yardSalezClientApp
+ * @description
+ * # yardSalezClientApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yardSalezClientApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngSanitize',
+    'ngTouch'
+  ]);
